225d2166d14ed4c37d04e9173ed3f4a01266a08a
Timo Eichelmann <teichelm@c4b2c6.42berlin.de>
2024-11-01 15:40:22 +0100

    f

---
+++ ClapTrap.cpp
@@ -0,0 +1,23 @@
+#include "ClapTrap.h"
+    
+	ClapTrap::ClapTrap(void)
+	{
+		//Default Constructor
+	}
+
+    ClapTrap::ClapTrap(const ClapTrap& other)
+	{
+		//Copy Constructor
+	}
+
+    ClapTrap::ClapTrap& operator=(const ClapTrap& other)
+	{
+		//Copy Assignment Constructor
+	}
+
+	ClapTrap::~ClapTrap(void)
+	{
+		//Dstructor
+	}
+
+
---
+++ ClapTrap.h
@@ -0,0 +1,16 @@
+#ifndef CLAPTRAP_H
+#define CLAPTRAP_H
+
+class ClapTrap {
+public:
+    ClapTrap();
+
+    ClapTrap(const ClapTrap& other);
+
+    ClapTrap& operator=(const ClapTrap& other);
+
+private:
+
+};
+
+#endif // CLAPTRAP_H
